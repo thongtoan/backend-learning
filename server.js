c50e6c45081b880c2a2be15ef6589eca762deb81
thongtoan <n.thongtoan@gmail.com>
2025-05-24 10:09:00 +0700

    apply section3

---
+++ server.js
@@ -0,0 +1,11 @@
+import app from "./src/app.js";
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => console.log(`Server is closed`));
+});
---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+// inite db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompression = "HaHa";
+  return res.status(200).json({
+    message: "Welcome Toan!",
+    metadata: strCompression.repeat(10000),
+  });
+});
+// handling error
+
+export default app;
